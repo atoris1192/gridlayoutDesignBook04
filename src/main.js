7ab1fca8514741bc4bb44619c6ccb0ea2a03dcda
atoris1192 <atoris1192@gmail.com>
2019-01-09 12:32:42 +0900

    btn

---
+++ src/main.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', () => {
+  'use strict'
+
+  // console.log('-- main.js log --')
+
+  const openBtn = document.querySelector('.page-btn')
+  const closeBtn = document.querySelector('.page-btn-close')
+  const pageNav = document.querySelector('.page-nav')
+
+  openBtn.addEventListener('click', () => {
+    console.log('-- open btn --')
+    pageNav.classList.add('open')
+    openBtn.classList.add('on')
+    closeBtn.classList.add('on')
+  })
+  closeBtn.addEventListener('click', () => {
+    console.log('-- close btn --');
+    pageNav.classList.remove('open')
+    openBtn.classList.remove('on')
+    closeBtn.classList.remove('on')
+  })
+})
